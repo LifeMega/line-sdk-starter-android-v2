13787b3ff4d1ae7bf63b02e41d1f46b95566d3b4
LifeMega <30980829+LifeMega@users.noreply.github.com>
2017-08-19 03:46:20 +0700

    Update Constants.java

--- app/src/main/java/jp/line/android/sdk/sample/Constants.java
+++ app/src/main/java/jp/line/android/sdk/sample/Constants.java
@@ -3,5 +3,5 @@ package jp.line.android.sdk.sample;
 public class Constants {C:\Users\Own\line-sdk-starter-android-v2\gradle
 
     // Set this value to your Channel ID
-    public static final String CHANNEL_ID = "";
+    public static final String CHANNEL_ID = "1530731911";
 }
